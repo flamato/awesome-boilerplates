6847daef5a68155ccd7deaae1079b56b7b064889
Filipe Ximenes <filipeximenes@gmail.com>
2017-03-31 11:14:30 -0300

    removes static pages

--- urls.py
+++ urls.py
@@ -34,12 +34,12 @@ urlpatterns = patterns("",
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, 'logout',),
 
     # static pages
-    url(r"^about/$", TemplateView.as_view(template_name='pages/faq.html'), name="about"),
-    url(r"^terms/$", TemplateView.as_view(template_name='pages/terms.html'), name="terms"),
-    url(r"^faq/$", TemplateView.as_view(template_name='pages/faq.html'), name="faq"),
-    url(r"^syndication/$", TemplateView.as_view(template_name='pages/syndication.html'), name="syndication"),
-    url(r"^contribute/$", TemplateView.as_view(template_name='pages/contribute.html'), name="contribute"),
-    url(r"^help/$", TemplateView.as_view(template_name='pages/help.html'), name="help"),
+    # url(r"^about/$", TemplateView.as_view(template_name='pages/faq.html'), name="about"),
+    # url(r"^terms/$", TemplateView.as_view(template_name='pages/terms.html'), name="terms"),
+    # url(r"^faq/$", TemplateView.as_view(template_name='pages/faq.html'), name="faq"),
+    # url(r"^syndication/$", TemplateView.as_view(template_name='pages/syndication.html'), name="syndication"),
+    # url(r"^contribute/$", TemplateView.as_view(template_name='pages/contribute.html'), name="contribute"),
+    # url(r"^help/$", TemplateView.as_view(template_name='pages/help.html'), name="help"),
 
     # new apps
     url(r"^search/", include("searchv2.urls")),
